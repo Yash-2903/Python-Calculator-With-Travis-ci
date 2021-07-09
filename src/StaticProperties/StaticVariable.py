cfdc347402d41cee6d241c80a76abd3c4391a067
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-09 00:04:54 -0500

    Update: Modified StaticVariable.py file

--- src/StaticProperties/StaticVariable.py
+++ src/StaticProperties/StaticVariable.py
@@ -1,6 +1,6 @@
 # Assign Static variable to Static Text
 class StaticVariable:
-    unitTestAddition = "UnitTest/Data/Unit_Test_Addition.csv"
+    unitTestAddition = "src/UnitTest/Data/Unit_Test_Addition.csv"
     unitTestSubtraction = "/Data/Unit_Test_Subtraction.csv"
     unitTestMultiplication = "/src/UnitTest/Data/Unit_Test_Multiplication.csv"
     unitTestDivision = "/src/UnitTest/Data/Unit_Test_Division.csv"
