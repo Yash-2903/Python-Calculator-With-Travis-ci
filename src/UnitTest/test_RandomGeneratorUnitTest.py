d60255d03e098d8e72fd943e1d3bed0b09ecc041
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 18:33:03 -0500

    Update: Added Function for Decimal and Integer with seed to test the value

--- src/UnitTest/test_RandomGeneratorUnitTest.py
+++ src/UnitTest/test_RandomGeneratorUnitTest.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
         self.start = 1
         self.end = 100
         self.length = 6
-        self.seed = 8
+        self.seed = 5
         self.num_val = 3
         # self.list = self.random.random_integer_list(self.start, self.end, self.length, self.seed)
 
@@ -19,8 +19,14 @@ class MyTestCase(unittest.TestCase):
 
     def test_random_without_seed_integer(self):
         int_random = self.random.random_without_seed_integer(self.start, self.end)
-        self.assertEqual(isinstance(int_random, int), True)
+        pprint(int_random)
+        self.assertEqual(isinstance(self.random.random_without_seed_integer(self.start, self.end), int), True)
 
     def test_random_without_seed_decimal(self):
         decimal_random = self.random.random_without_seed_decimal(self.start, self.end)
-        self.assertEqual(isinstance(decimal_random, float), True)
+        pprint(decimal_random)
+        self.assertEqual(isinstance(self.random.random_without_seed_decimal(self.start, self.end), float), True)
+
+    def test_random_with_seed_integer(self):
+        int_rd_seed = self.random.random_with_seed_integer(self.start, self.end, self.seed)
+        self.assertEqual(self.random.random_with_seed_integer(self.start, self.end, self.seed), int_rd_seed)
