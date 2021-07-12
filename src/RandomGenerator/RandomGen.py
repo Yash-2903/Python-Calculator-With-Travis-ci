29701e03248fe52042179f6870a18372c62e65b7
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 18:32:14 -0500

    Update: Added Function for Decimal and Integer with seed

--- src/RandomGenerator/RandomGen.py
+++ src/RandomGenerator/RandomGen.py
@@ -1,14 +1,28 @@
 from src.RandomGenerator.RandomGenWithoutSeedDecimal import random_without_seed_decimal
 from src.RandomGenerator.RandomGenWithoutSeedInteger import random_without_seed_integer
+from src.RandomGenerator.RandomGenWithSeedDecimal import random_with_seed_decimal
+from src.RandomGenerator.RandomGenWithSeedInteger import random_with_seed_integer
 
 
 class Random:
     result = 0
 
+
     def random_without_seed_decimal(self, start, end):
         self.result = random_without_seed_decimal(start, end)
         return self.result
 
+
     def random_without_seed_integer(self, start, end):
         self.result = random_without_seed_integer(start, end)
+        return self.result
+
+
+    def random_with_seed_decimal(self, start, end, seed):
+        self.result = random_with_seed_decimal(start, end, seed)
+        return self.result
+
+
+    def random_with_seed_integer(self, start, end, seed):
+        self.result = random_with_seed_integer(start, end, seed)
         return self.result
