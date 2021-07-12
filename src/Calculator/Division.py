258ba03449103c9c471c3a8664b36709ef7387dd
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 00:27:35 -0500

    Update: Modified Division file with adding exception

--- src/Calculator/Division.py
+++ src/Calculator/Division.py
@@ -1,2 +1,7 @@
 def division(a, b):
-    return int(b) / int(a)
+    try:
+        div = float(b) / float(a)
+        return round(div, 9)
+    except ZeroDivisionError as error:
+        error = "ERROR!  YOU CANNOT DIVIDE BY ZERO!"
+        return error
