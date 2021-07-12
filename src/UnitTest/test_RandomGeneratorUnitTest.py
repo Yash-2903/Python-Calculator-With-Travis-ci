0da85d53779f2dbad3c97e65501a211b9640e484
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 18:55:16 -0500

    Update: Added function for decimal and Integer seed List for test run

--- src/UnitTest/test_RandomGeneratorUnitTest.py
+++ src/UnitTest/test_RandomGeneratorUnitTest.py
@@ -8,10 +8,9 @@ class MyTestCase(unittest.TestCase):
         self.random = Random()
         self.start = 1
         self.end = 100
-        self.length = 6
+        self.length = 3
         self.seed = 5
         self.num_val = 3
-        # self.list = self.random.random_integer_list(self.start, self.end, self.length, self.seed)
 
     def test_instantiate_calculator_self(self):
         self.assertIsInstance(self.random, Random)
@@ -30,3 +29,17 @@ class MyTestCase(unittest.TestCase):
     def test_random_with_seed_integer(self):
         int_rd_seed = self.random.random_with_seed_integer(self.start, self.end, self.seed)
         self.assertEqual(self.random.random_with_seed_integer(self.start, self.end, self.seed), int_rd_seed)
+
+    def test_random_integer_list(self):
+        int_array = self.random.random_with_seed_list_integer(self.start, self.end, self.length, self.seed)
+        pprint(int_array)
+        for val in int_array:
+            test_value = int(val)
+            self.assertEqual(isinstance(test_value, int), True)
+
+    def test_random_decimal_list(self):
+        decimal_array = self.random.random_with_seed_list_decimal(self.start, self.end, self.length, self.seed)
+        pprint(decimal_array)
+        for val in decimal_array:
+            test_value = float(val)
+            self.assertEqual(isinstance(test_value, float), True)
