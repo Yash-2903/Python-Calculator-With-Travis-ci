53c544e96cc5f2d2db400737ed85b6117bcc8cce
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 18:55:01 -0500

    Update: Added function for decimal and Integer seed List

--- src/RandomGenerator/RandomGen.py
+++ src/RandomGenerator/RandomGen.py
@@ -2,6 +2,8 @@ from src.RandomGenerator.RandomGenWithoutSeedDecimal import random_without_seed_
 from src.RandomGenerator.RandomGenWithoutSeedInteger import random_without_seed_integer
 from src.RandomGenerator.RandomGenWithSeedDecimal import random_with_seed_decimal
 from src.RandomGenerator.RandomGenWithSeedInteger import random_with_seed_integer
+from src.RandomGenerator.RandomGenWithSeedListInteger import random_with_seed_list_integer
+from src.RandomGenerator.RandomGenWithSeedListDecimal import random_with_seed_list_decimal
 
 
 class Random:
@@ -25,4 +27,12 @@ class Random:
 
     def random_with_seed_integer(self, start, end, seed):
         self.result = random_with_seed_integer(start, end, seed)
+        return self.result
+
+    def random_with_seed_list_decimal(self, start, end, length, seed):
+        self.result = random_with_seed_list_decimal(start, end, length, seed)
+        return self.result
+
+    def random_with_seed_list_integer(self, start, end, length, seed):
+        self.result = random_with_seed_list_integer(start, end, length, seed)
         return self.result
