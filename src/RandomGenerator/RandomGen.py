437d6a2aaade46dc975b6f9c48da3249c5db5b78
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 17:51:16 -0500

    Update: Modified the Function

--- src/RandomGenerator/RandomGen.py
+++ src/RandomGenerator/RandomGen.py
@@ -1,2 +1,14 @@
+from src.RandomGenerator.RandomGenWithoutSeedDecimal import random_without_seed_decimal
+from src.RandomGenerator.RandomGenWithoutSeedInteger import random_without_seed_integer
+
+
 class Random:
     result = 0
+
+    def random_decimal(self, start, end):
+        self.result = random_without_seed_decimal(start, end)
+        return self.result
+
+    def random_integer(self, start, end):
+        self.result = random_without_seed_integer(start, end)
+        return self.result
