47425e8630efe341e2591d8bf8e87197b95f3f85
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-12 00:30:26 -0500

    Update: Added New Statistic Unit Test file

---
+++ src/UnitTest/test_StatisticsUnitTest.py
@@ -0,0 +1,27 @@
+import unittest
+from src.Statistics.Statistics import Statistics
+from src.CSVReader.CSVReader import CSVReader
+from src.StaticProperties.StaticVariable import StaticVariable
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.statistics = Statistics()
+        self.test_stat_data = CSVReader(StaticVariable.unitTestStatistics).data
+        self.testData = [int(row['Value']) for row in self.test_stat_data]
+
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+
+    def test_decorator_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+
+    def test_mean_calculator(self):
+        self.assertEqual(self.statistics.mean(self.testData), 6.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
